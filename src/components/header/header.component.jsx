d39bbbd13a792fc89187976b6e6569a25285c1df
slapner <bclap4@gmail.com>
2019-12-27 14:42:49 -0500

    added cart icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { connect } from "react-redux";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+
 import "./header.styles.scss";
 
 const Header = ({ currentUser }) => (
@@ -27,6 +29,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
