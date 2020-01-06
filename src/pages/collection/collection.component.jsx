4b91764b2b1efdbf2b631f66c2595f98ecb54e65
slapner <bclap4@gmail.com>
2020-01-06 12:51:25 -0500

    wrapped collections-overview and collection in container HOC

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import {
   CollectionContainer,
   ItemContainer,
   TitleContainer
-} from "./collection.styles";
+} from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
