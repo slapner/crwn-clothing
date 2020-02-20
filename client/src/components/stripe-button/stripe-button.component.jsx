c4d3b14fdb120f244bfe1f57c4bb016d8f887d28
Brian Clapner <Bclapner@latlmacw163jgh8.man.co>
2020-02-20 10:44:50 -0500

    wired stripe to node backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,45 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = "pk_test_ZJtKuMF7eNfKGjLoYKP1LYop00mQSsbKdS";
+	const priceForStripe = price * 100;
+	const publishableKey = "pk_test_ZJtKuMF7eNfKGjLoYKP1LYop00mQSsbKdS";
 
-  const onToken = token => {
-    console.log(token);
-    alert("Payment successful!");
-  };
+	const onToken = token => {
+		axios({
+			url: "payment",
+			method: "post",
+			data: {
+				amount: priceForStripe,
+				token
+			}
+		})
+			.then(response => {
+				alert("Payment successful");
+			})
+			.catch(error => {
+				console.log("Payment error: ", JSON.parse(error));
+				alert(
+					"There was an issue with your payment. Please use the provided credit card"
+				);
+			});
+	};
 
-  return (
-    <StripeCheckout
-      label="Pay Now"
-      name="CRWN Clothing Ltd."
-      billingAddress
-      shippingAddress
-      image="http://svgshare.com/i/CUz.svg"
-      description={`Your total is $${price}`}
-      amount={priceForStripe}
-      panelLabel="Pay Now"
-      token={onToken}
-      stripeKey={publishableKey}
-    />
-  );
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="CRWN Clothing Ltd."
+			billingAddress
+			shippingAddress
+			image="http://svgshare.com/i/CUz.svg"
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
 };
 
 export default StripeCheckoutButton;
